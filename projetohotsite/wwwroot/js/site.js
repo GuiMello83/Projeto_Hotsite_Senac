6b8d6675491ce21ccf578a6e4c24f6d4a8aa50b7
GuiMello83 <gui.vazdemello@gmail.com>
2021-12-20 07:33:07 -0300

    Resolvido

---
+++ projetohotsite/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('#formInteresse').submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+        url: '/Home/Cadastrar',
+        data: $('#formInteresse').serialize(),
+        type: 'POST',
+        success: function (e) {
+            $('#modalCadastroSucesso').modal('show');
+        },
+        error: function (e) {
+            $('#modalCadastroFalha').modal('show');
+        }
+    });
+})
+
+$('#eventos').on('click', function (e) {
+    $('#modalEventos').modal('show');
+    e.preventDefault();
+});
+
+$('#dicas').on('click', function (e) {
+    $('#modalDicas').modal('show');
+    e.preventDefault();
+});
+
+$('#apoiadores').on('click', function (e) {
+    $('#modalApoiadores').modal('show');
+    e.preventDefault();
+});
